b3dba6170acf184bbdcaacb78fc86a37330a90c3
Asmita  Mahajan <92370715+AsmitaAM@users.noreply.github.com>
2022-02-10 07:30:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include('partials/menu.php');
+?>
+        <!-- menu section ends  -->
+        <!-- main content section start  -->
+        <div class = "main-contenet">
+        <div class= "wrapper">
+            <h1>DASHBOARD</strong></h1>
+            <div class="col-4">
+                <h1>5</h1><br>
+                categories  
+            </div>
+            <div class="col-4">
+                <h1>5</h1><br>
+                categories  
+            </div>
+            <div class="col-4">
+                <h1>5</h1><br>
+                categories  
+            </div>
+            <div class="col-4">
+                <h1>5</h1><br>
+                categories  
+            </div>
+            <div class ="clearfix" > </div>
+        </div>
+             
+        </div>
+        <!-- main content section ends  -->
+ <?php include('partials/footer.php');
+ ?> 
+        
+        
